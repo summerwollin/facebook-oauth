6cb3a929b6243f30814cd444784268d96cce1009
Summer Wollin <summer.wollin@gmail.com>
2016-04-03 14:06:28 -0600

    fixing errors

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(process.env.HOST);
   res.render('index', { title: 'Express' });
 });
 
